47b025e354c86ecf252939d474cac6a8ebc967b0
hakan_karakaya <hakan.karakaya@sebit.com.tr>
2018-01-08 17:53:23 +0300

    first changes.

--- PhotoSearch/DownloadViewController.swift
+++ PhotoSearch/DownloadViewController.swift
@@ -1,9 +1,5 @@
 //
-//  ViewController.swift
-//  DownloadTaskWithNSURLSession
-//
-//  Created by Malek T. on 11/4/15.
-//  Copyright © 2015 Medigarage Studios LTD. All rights reserved.
+//  DownloadViewController.swift
 //
 
 import UIKit
@@ -13,27 +9,30 @@ class DownloadViewController: UIViewController, URLSessionDownloadDelegate, UIDo
     var downloadTask: URLSessionDownloadTask!
     var backgroundSession: URLSession!
 
-    @IBAction func startDownload(_ sender: AnyObject) {
-        let url = URL(string: "http://www.pcarsiv.com/Food101.zip")!
-        downloadTask = backgroundSession.downloadTask(with: url)
-        downloadTask.resume()
-    }
-    @IBAction func pause(_ sender: AnyObject) {
-        if downloadTask != nil{
-            downloadTask.suspend()
-        }
-    }
-    @IBAction func resume(_ sender: AnyObject) {
-        if downloadTask != nil{
-            downloadTask.resume()
-        }
-    }
-    @IBAction func cancel(_ sender: AnyObject) {
-        if downloadTask != nil{
-            downloadTask.cancel()
-        }
-    }
+//    @IBAction func startDownload(_ sender: AnyObject) {
+//        let url = URL(string: "http://www.pcarsiv.com/cars.zip")!
+//        downloadTask = backgroundSession.downloadTask(with: url)
+//        downloadTask.resume()
+//    }
+//    @IBAction func pause(_ sender: AnyObject) {
+//        if downloadTask != nil{
+//            downloadTask.suspend()
+//        }
+//    }
+//    @IBAction func resume(_ sender: AnyObject) {
+//        if downloadTask != nil{
+//            downloadTask.resume()
+//        }
+//    }
+//    @IBAction func cancel(_ sender: AnyObject) {
+//        if downloadTask != nil{
+//            downloadTask.cancel()
+//        }
+//    }
+    
     @IBOutlet var progressView: UIProgressView!
+    @IBOutlet weak var statusLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,11 +40,48 @@ class DownloadViewController: UIViewController, URLSessionDownloadDelegate, UIDo
         backgroundSession = Foundation.URLSession(configuration: backgroundSessionConfiguration, delegate: self, delegateQueue: OperationQueue.main)
         progressView.setProgress(0.0, animated: false)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        CheckModelFile();
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func CheckModelFile() {
+        let path = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+        let documentDirectoryPath:String = path[0]
+        let fileManager = FileManager()
+        let destinationURLForFile = URL(fileURLWithPath: documentDirectoryPath.appendingFormat("/cars.zip"))
+        
+        if fileManager.fileExists(atPath: destinationURLForFile.path){
+            let uiAlert = UIAlertController(title: "Blue Eye", message: "Found the model. Download again?", preferredStyle: UIAlertControllerStyle.alert)
+            self.present(uiAlert, animated: true, completion: nil)
+            
+            uiAlert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { action in
+                self.statusLabel.text = "Downloading model please wait..."
+                let url = URL(string: "http://www.pcarsiv.com/cars.zip")!
+                self.downloadTask = self.backgroundSession.downloadTask(with: url)
+                self.downloadTask.resume()
+            }))
+            uiAlert.addAction(UIAlertAction(title: "No", style: .cancel, handler: { action in
+                self.statusLabel.text = "Found the model."
+                self.progressView.setProgress(1.0, animated: true)
+                
+                if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController") as? ViewController
+                {
+                    self.present(vc, animated: true, completion: nil)
+                }
+            }))
+        }else{
+            statusLabel.text = "Downloading model please wait..."
+            let url = URL(string: "http://www.pcarsiv.com/cars.zip")!
+            downloadTask = backgroundSession.downloadTask(with: url)
+            downloadTask.resume()
+        }
+    }
     
     func showFileWithPath(path: String){
         let isFileFound:Bool? = FileManager.default.fileExists(atPath: path)
@@ -53,7 +89,6 @@ class DownloadViewController: UIViewController, URLSessionDownloadDelegate, UIDo
             let viewer = UIDocumentInteractionController(url: URL(fileURLWithPath: path))
             viewer.delegate = self
             viewer.presentPreview(animated: true)
-            let imagePath = (self.getDirectoryPath() as NSString)
         }
     }
     
@@ -71,21 +106,12 @@ class DownloadViewController: UIViewController, URLSessionDownloadDelegate, UIDo
         
         let path = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
         let documentDirectoryPath:String = path[0]
-        
-//        do {
-//            let path = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
-//            let documentDirectory = URL(fileURLWithPath: path[0])
-//            let originPath = documentDirectory.appendingPathComponent("Food101.zip")
-//            let destinationPath = documentDirectory.appendingPathComponent("Food101.mlmodel")
-//            try FileManager.default.moveItem(at: originPath, to: destinationPath)
-//        } catch {
-//            print(error)
-//        }
-       
         let fileManager = FileManager()
-        let destinationURLForFile = URL(fileURLWithPath: documentDirectoryPath.appendingFormat("/Food101.zip"))
+        let destinationURLForFile = URL(fileURLWithPath: documentDirectoryPath.appendingFormat("/cars.zip"))
         
         if fileManager.fileExists(atPath: destinationURLForFile.path){
+            statusLabel.text = "Found the model."
+            progressView.setProgress(1.0, animated: true)
             if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController") as? ViewController
             {
                 present(vc, animated: true, completion: nil)
--- PhotoSearch/ViewController.swift
+++ PhotoSearch/ViewController.swift
@@ -8,116 +8,96 @@ import Vision
 import AVFoundation
 
 class ViewController: UIViewController, FrameExtractorDelegate {
- 
-  var frameExtractor: FrameExtractor!
-  
-  @IBOutlet weak var previewImage: UIImageView!
-  @IBOutlet weak var iSee: UILabel!
-
-  var settingImage = false
     
-  var currentImage: CIImage? {
-    didSet {
-      if let image = currentImage{
-        self.detectScene(image: image)
-      }
-    }
-  }
+    var frameExtractor: FrameExtractor!
     
-    func downloadComplete(){
-        
-        
-    }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    frameExtractor = FrameExtractor()
-    frameExtractor.delegate = self
-  }
-      
-  func captured(image: UIImage) {
-    self.previewImage.image = image
-    if let cgImage = image.cgImage, !settingImage {
-      settingImage = true
-      DispatchQueue.global(qos: .userInteractive).async {[unowned self] in
-        self.currentImage = CIImage(cgImage: cgImage)
-      }
-    }
-  }
-
-  func addEmoji(id: String) -> String {
-    switch id {
-    case "pizza":
-      return "🍕"
-    case "hot dog":
-      return "🌭"
-    case "chicken wings":
-      return "🍗"
-    case "french fries":
-      return "🍟"
-    case "sushi":
-      return "🍣"
-    case "chocolate cake":
-      return "🍫🍰"
-    case "donut":
-      return "🍩"
-    case "spaghetti bolognese":
-      return "🍝"
-    case "caesar salad":
-      return "🥗"
-    case "macaroni and cheese":
-      return "🧀"
-    default:
-      return ""
-    }
-  }
-  func detectScene(image: CIImage) {
-    do {
-        // You have to add the file whose name is changed from KerasMNIST.mlmodel to KerasMNIST.bin to app target.
-        // In order to avoid that Xcode compiles a mlmodel file automatcally.
-        let documentDirectoryURL =  try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-        let compiledURL = try MLModel.compileModel(at: documentDirectoryURL.appendingPathComponent("Food101.zip"))
-        
-        let coremlModel = try MLModel(contentsOf: compiledURL)
-
-        guard let model = try? VNCoreMLModel(for: coremlModel) else {
-            fatalError()
-        }
-        let request = VNCoreMLRequest(model: model) { [unowned self] request, error in
-            guard let results = request.results as? [VNClassificationObservation],
-                let _ = results.first else {
-                    self.settingImage = false
-                    return
+    @IBOutlet weak var previewImage: UIImageView!
+    @IBOutlet weak var iSee: UILabel!
+    
+    var settingImage = false
+    var compiledURL:URL!
+    
+    var currentImage: CIImage? {
+        didSet {
+            if let image = currentImage{
+                self.detectScene(image: image)
             }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        do{
+            let documentDirectoryURL =  try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            compiledURL = try MLModel.compileModel(at: documentDirectoryURL.appendingPathComponent("cars.zip"))
+        }catch{
+            print(error)
+        }
+        
+        frameExtractor = FrameExtractor()
+        frameExtractor.delegate = self
+    }
+    
+    func captured(image: UIImage) {
+        self.previewImage.image = image
+        if let cgImage = image.cgImage, !settingImage {
+            settingImage = true
+            DispatchQueue.global(qos: .userInteractive).async {[unowned self] in
+                self.currentImage = CIImage(cgImage: cgImage)
+            }
+        }
+    }
+    
+    func detectScene(image: CIImage) {
+        do {
+            // You have to add the file whose name is changed from KerasMNIST.mlmodel to KerasMNIST.bin to app target.
+            // In order to avoid that Xcode compiles a mlmodel file automatcally.
+//            let documentDirectoryURL =  try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+//            let compiledURL = try MLModel.compileModel(at: documentDirectoryURL.appendingPathComponent("cars.zip"))
             
-            DispatchQueue.main.async { [unowned self] in
-                if let first = results.first {
-                    if Int(first.confidence * 100) > 1 {
-                        self.iSee.text = "I see \(first.identifier) \(self.addEmoji(id: first.identifier))"
+            let coremlModel = try MLModel(contentsOf: compiledURL)
+            
+            guard let model = try? VNCoreMLModel(for: coremlModel) else {
+                fatalError()
+            }
+            let request = VNCoreMLRequest(model: model) { [unowned self] request, error in
+                guard let results = request.results as? [VNClassificationObservation],
+                    let _ = results.first else {
                         self.settingImage = false
-                    }
+                        return
                 }
-                //        results.forEach({ (result) in
-                //          if Int(result.confidence * 100) > 1 {
-                //            self.settingImage = false
-                //            print("\(Int(result.confidence * 100))% it's \(result.identifier) ")
-                //          }
-                //        })
-                // print("********************************")
                 
+                DispatchQueue.main.async { [unowned self] in
+                    if let first = results.first {
+                        if Int(first.confidence * 100) > 1 {
+                            self.iSee.text = "I see \(first.identifier)"
+                            self.settingImage = false
+                        }
+                    }
+                    //        results.forEach({ (result) in
+                    //          if Int(result.confidence * 100) > 1 {
+                    //            self.settingImage = false
+                    //            print("\(Int(result.confidence * 100))% it's \(result.identifier) ")
+                    //          }
+                    //        })
+                    // print("********************************")
+                    
+                }
             }
-        }
-        let handler = VNImageRequestHandler(ciImage: image)
-        DispatchQueue.global(qos: .userInteractive).async {
-            do {
-                try handler.perform([request])
-            } catch {
-                print(error)
+            let handler = VNImageRequestHandler(ciImage: image)
+            DispatchQueue.global(qos: .userInteractive).async {
+                do {
+                    try handler.perform([request])
+                } catch {
+                    print(error)
+                }
             }
+        } catch {
+            print(error)
         }
-    } catch {
-        print(error)
     }
-  }
 }
 
